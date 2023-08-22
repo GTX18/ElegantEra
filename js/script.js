464df64f2d5910fd439b10d0117f9cc026a0391f
GTX18 <abhishekhaldankar18@gmail.com>
2023-08-22 21:48:10 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,106 @@
+let navbar = document.querySelector(".navbar");
+
+document.querySelector('#menu-bar').onclick = () =>{
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
+
+
+var swiper = new Swiper (".featured-slider", {
+    loop:true,
+    grabCursor:true,
+    spaceBetween: 20,
+    autoplay:{
+        delay:5500,
+        disableOnInteraction: false,
+    },
+    navigation:{
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints:{
+        0:{
+            slidesPerView: 1,
+        },
+        768:{
+            slidesPerView: 2,
+        },
+        991:{
+            slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper (".featured-slider-2", {
+    loop:true,
+    grabCursor:true,
+    spaceBetween: 20,
+    autoplay:{
+        delay:8500,
+        disableOnInteraction: false,
+    },
+    navigation:{
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints:{
+        0:{
+            slidesPerView: 1,
+        },
+        768:{
+            slidesPerView: 2,
+        },
+        991:{
+            slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper (".featured-slider-3", {
+    loop:true,
+    grabCursor:true,
+    spaceBetween: 20,
+    autoplay:{
+        delay:6500,
+        disableOnInteraction: false,
+    },
+    navigation:{
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints:{
+        0:{
+            slidesPerView: 1,
+        },
+        768:{
+            slidesPerView: 2,
+        },
+        991:{
+            slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper (".brand-slider", {
+    loop:true,
+    grabCursor:true,
+    spaceBetween: 20,
+    autoplay:{
+        delay:2500,
+        disableOnInteraction: false,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView: 2,
+        },
+        768:{
+            slidesPerView: 3,
+        },
+        991:{
+            slidesPerView: 5,
+        },
+    },
+});
